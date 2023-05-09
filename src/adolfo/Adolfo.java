d70803554c5a95723f223c8b03e79f9cadf6e4db
josems14 <17997522@alu.murciaeduca.es>
2023-05-09 16:01:19 +0200

    Ejercicio 2 JuezLTI

--- src/adolfo/Adolfo.java
+++ src/adolfo/Adolfo.java
@@ -3,6 +3,8 @@ public class Adolfo {
     public static void main(String[] args) {
         System.out.println( "Inicio Proyecto");
         System.out.println("Ejercicio 1 JuezLTI");
+        System.out.println("Ejercicio 2 JuezLTI");
+        
     }
     
 }
